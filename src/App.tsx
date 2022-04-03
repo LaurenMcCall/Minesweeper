c596b0d739c92eb377421dc8d1c03a3c6fb443a7
Lauren McCall <lmmccolloch@gmail.com>
2022-04-03 14:08:57 -0400

    makes a new game state from the API

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 export function App() {
   const [game, setGame] = useState({
-    id: 1,
+    id: undefined,
     board: [
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -13,15 +13,38 @@ export function App() {
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
       [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
     ],
-    state: 'new',
-    mines: 10,
+    state: undefined,
+    mines: undefined,
   })
+
+  async function handleNewEasyGame() {
+    const gameDifficultyOptions = { difficulty: 0 }
+
+    const url = 'https://minesweeper-api.herokuapp.com/games'
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(gameDifficultyOptions),
+    }
+
+    const response = await fetch(url, fetchOptions)
+    console.log(response)
+    if (response.ok) {
+      const newGameStateJson = await response.json()
+
+      setGame(newGameStateJson)
+    }
+  }
+
   return (
     <main>
       <h1>Minesweeper</h1>
       <h2>
-        <button>New Easy Game</button>
+        <button onClick={handleNewEasyGame}>New Easy Game</button>
       </h2>
+      <h3>Game #: {game.id}</h3>
+      <h3>Mines: {game.mines}</h3>
 
       <ul>
         {game.board.map(function (gameRow) {
