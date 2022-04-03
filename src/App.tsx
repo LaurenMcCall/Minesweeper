4f876afd028852eba6370e12038a80c30c6146e7
Lauren McCall <lmmccolloch@gmail.com>
2022-04-03 16:49:30 -0400

    handles different game difficulties and more css

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,10 @@ export function App() {
     state: undefined,
     mines: undefined,
   })
+  const [difficulty, setDifficulty] = useState<0 | 1 | 2>(0)
 
-  async function handleNewEasyGame() {
-    const gameDifficultyOptions = { difficulty: 0 }
+  async function handleNewGame(newGameDifficulty: 0 | 1 | 2) {
+    const gameDifficultyOptions = { difficulty: newGameDifficulty }
 
     const url = 'https://minesweeper-api.herokuapp.com/games'
 
@@ -33,6 +34,7 @@ export function App() {
     if (response.ok) {
       const newGameStateJson = await response.json()
 
+      setDifficulty(newGameDifficulty)
       setGame(newGameStateJson)
     }
   }
@@ -75,11 +77,14 @@ export function App() {
     if (value === '*') {
       return <i className="fa fa-bomb" />
     }
+    if (value === '@') {
+      return <i className="fa fa-flag" />
+    }
     return value
   }
 
-  function transformCellClassName(value: string) {
-    if (value === 'F') {
+  function transformCellClassName(value: string | number) {
+    if (value === 'F' || value === '@') {
       return 'cell-flag'
     }
     if (value === '*') {
@@ -88,6 +93,9 @@ export function App() {
     if (value === '_') {
       return 'cell-free'
     }
+    if ([1, 2, 3, 4, 5, 6, 7, 8].includes(Number(value))) {
+      return `cell-number cell-${value}`
+    }
     return undefined
   }
 
@@ -95,12 +103,14 @@ export function App() {
     <main>
       <h1>Minesweeper</h1>
       <h2>
-        <button onClick={handleNewEasyGame}>New Easy Game</button>
+        <button onClick={() => handleNewGame(0)}>New Easy Game</button>
+        <button onClick={() => handleNewGame(1)}>New Intermediate Game</button>
+        <button onClick={() => handleNewGame(2)}>New Difficult Game</button>
       </h2>
       <h3>Game #: {game.id}</h3>
       <h3>Mines: {game.mines}</h3>
 
-      <section>
+      <section className={`difficulty-${difficulty}`}>
         {game.board.map(function (gameRow, row) {
           return gameRow.map(function (square, col) {
             return (
